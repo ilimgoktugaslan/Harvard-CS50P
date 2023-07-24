274dd03478add9c34b431a915a0a62d41dc5c4ac
İlim Göktuğ Aslan (200412087) <139969030+ilimgoktugaslan@users.noreply.github.com>
2023-07-24 20:27:50 +0300

    Harvard University CS50P
    My studies for the Harvard University CS50P course.

---
+++ CS50P/Functions, Variables/Functions, Variables/Functions, Variables/Functions__Variables.py
@@ -0,0 +1,70 @@
+"""
+# Ask user for their name
+# Remove whitespace from str and capitalize user's name
+# name= input("What's your name \n " ).strip().title()
+
+# Say hello to user
+# print("Hello,",name) 
+
+# Remove whitespace from str and capitalize user's name
+# name=name.strip().title()
+
+# Split user's name into first name and last name 
+first,last=name.split(" ")
+
+# Capitalize user's name
+# name=name.capitalize()
+
+# Title user's name 
+# name=name.title()
+
+print(f"Hello {first}") 
+# print("hello,\"friend\"")
+"""
+
+# Calculator
+"""
+x =int(input("What's x?"))
+y =int(input("What's y?"))
+# print(int(input("What's x?"))+int(input("What's y?")))
+"""
+"""
+x =float(input("What's x?"))
+y =float(input("What's y?"))
+# z=round(x+y) # Rounds numbers to the nearest number
+# z=round(x/y,2) # There will be 2 digits after (,)
+z=(x/y)
+print(f"{z:.2f}")
+"""
+
+"""
+def hello(to="world"): # We created a founction on this line.
+    print("hello,",to)
+
+name=input("What's your name")
+hello(name)
+"""
+"""
+def main():
+    name=input("What's your name")
+    hello()
+
+def hello(to="world"):
+    print("hello,",to
+
+main()
+"""
+"""
+def main():
+    x=int(input("What's x?"))
+    print("x square is",square(x))
+
+def square(n):
+    return pow (n,2)
+
+main()
+"""
+
+
+
+
